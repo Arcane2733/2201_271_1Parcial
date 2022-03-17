e5fe3a4dca9ab39232d2b8ecbcd229e0b0d2ffde
Arcane2733 <101817793+Arcane2733@users.noreply.github.com>
2022-03-17 11:48:11 -0500

    Código de promedio con for

--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 
-using namespace std; 
+using namespace std;
 
 int main(int argc, char** argv) {
+	
 	int l=0;
 	int n;
 	int nmr;
 	int s=0;
-	int p;
+	long p;
 	cout<< "ingrese la cantidad de veces a repetir"<<endl;
 	cin>> n;
-	while(l<n){
+	for(l=0;l<n; l++){
 		cout<< "inserte un numero"<<endl;
 		cin>>nmr;
 		s=s+nmr;
-		l++;
 	}
 	p=s/n;
 	cout<< "el promedio es:"<<endl;
 	cout<< p;
+
 	return 0;
 }
