cd292749c2c7c297d64c7b1e5c51b76b1af716d2
Jorge Osorio <101817793+Arcane2733@users.noreply.github.com>
2022-03-24 11:43:10 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,22 +3,35 @@
 using namespace std;
 
 int main(int argc, char** argv) {
+	int a;
+	int b;
+	int c;
+	int e;
+	int otro;
+	do{
+		cout<<"intrese el numero incial ";
+		cin>>a;
+		cout<<"ingrese hasta donde contar ";
+		cin>>b;
+		c=a;
+		for(int i = a+1; i <= b; i++){
+		
+			if(i%2==0){
+				e=1;
+			}
+				
+			else{
+				e=-1;
+			}
+		    c=c+(i*e);	
+		    cout<<e<<" "<<i<<" "<<c<<endl;
+		}	
+		cout<<"el resultado es: ";
+		cout<<c <<endl;
+		cout<<"si quiere hacer otro ingrese 1"<<endl;
+		cin>>otro;
+	}while(otro==1)
 	
-	int l=0;
-	int n;
-	int nmr;
-	int s=0;
-	long p;
-	cout<< "ingrese la cantidad de veces a repetir"<<endl;
-	cin>> n;
-	for(l=0;l<n; l++){
-		cout<< "inserte un numero"<<endl;
-		cin>>nmr;
-		s=s+nmr;
-	}
-	p=s/n;
-	cout<< "el promedio es:"<<endl;
-	cout<< p;
-
+	;
 	return 0;
 }
